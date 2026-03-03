599a90aaaa7f98dc2ef01864b22e4abafa099ad5
garretts8 <gar21085@byui.edu>
2026-03-02 17:52:27 -0700

    Setting up server, and checking that it works.

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-const express = require('express');
+const  express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.get("/", (req, res) => {
+    res.send("Hello Mitchelle");
+});
+
+app.listen(PORT);
+console.log("Web Server is listening at port "+ (process.env.port || PORT));
